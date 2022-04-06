6790682e050f0e9412055115f5c3db7cb9e63b4d
aimeirak <aimeirak10@gmail.com>
2022-04-06 07:02:11 +0200

    initial commit

---
+++ consoleSQLite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleSQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
